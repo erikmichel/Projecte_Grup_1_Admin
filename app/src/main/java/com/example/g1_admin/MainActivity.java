145ec15f341dd4468b8ffd9dd37367e4fda5fbfb
Paul <paugermaisar@hotmail.es>
2021-11-17 17:23:52 +0100

    Login Screen Branch First Commit

--- app/src/main/java/com/example/g1_admin/MainActivity.java
+++ app/src/main/java/com/example/g1_admin/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.g1_admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText txtUsername = findViewById(R.id.txtUsername);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                if (txtUsername.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin")) {
+                    //Login succeed
+
+                }
+            }
+        });
     }
 }
