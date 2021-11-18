8960ae962a99a11778dd7dd4236b60ccaa25e85f
Paul <paugermaisar@hotmail.es>
2021-11-18 15:25:55 +0100

    Finished login screen

--- app/src/main/java/com/example/g1_admin/MainActivity.java
+++ app/src/main/java/com/example/g1_admin/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.g1_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Intent intent = new Intent(this, MainMenu.class);
+
         EditText txtUsername = findViewById(R.id.txtUsername);
         EditText txtPassword = findViewById(R.id.txtPassword);
         Button btnLogin = findViewById(R.id.btnLogin);
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (txtUsername.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin")) {
                     //Login succeed
-
+                    //startActivity(intent);
                 }
             }
         });
