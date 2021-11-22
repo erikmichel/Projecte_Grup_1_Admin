223cd2d3a91a8245b253b7adbdf053eb14823e67
Edwing <cf18edwing.montero@iesjoandaustria.org>
2021-11-22 16:40:14 +0100

    form Activity/Fragment alone!

--- app/src/main/java/com/example/g1_admin/MainActivity.java
+++ app/src/main/java/com/example/g1_admin/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.g1_admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new formFragment()).commit();
     }
 }
