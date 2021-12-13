884949d467839d5b9abf2884054856213d68c644
Edwing <cf18edwing.montero@iesjoandaustria.org>
2021-12-13 15:54:24 +0100

    PreferencesScreen

---
+++ app/src/main/java/com/example/g1_admin/Controllers/Fragment/SettingsFragment.java
@@ -0,0 +1,13 @@
+package com.example.g1_admin.Controllers.Fragment;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
